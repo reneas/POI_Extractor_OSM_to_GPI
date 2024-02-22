065415339deaf1002ce2010892056ff23b1a0867
reneas <28440614+reneas@users.noreply.github.com>
2024-02-22 02:12:07 +0100

    moved dict to yaml, added functions, changed to path from os

--- README.md
+++ README.md
@@ -5,12 +5,13 @@ you will have to install the follwing external tools:
 -  [`osmosis`](https://github.com/openstreetmap/osmosis)
 -  [`gpsbabel`](https://github.com/GPSBabel/gpsbabel)
 
+- the script `filter_nodes.py` has to be located in the same directory as your `*.pbf` map file. This is going to be your `working directory`
+
 ## Steps to use:
 
 #### 1. Download desired area as `*.pbf` mapfile from https://extract.bbbike.org/
-#### 2. Set your `working_path` and `pbf_file_name`
-- `working_path` is the directory where your `*.pbf` file is located
-#### 3. Customize `poi_dict`:
+#### 2. Set your `pbf_file_name`
+#### 3. Customize `POIs.yaml`:
 - Define POI categories
 - Define a radius (in meter) around each POI in which other POIs of the same category are deleted. Set this to a really really large number to keep all POIs
 - Define `key`s and `values`s of [Map Features](https://wiki.openstreetmap.org/wiki/Map_features) that you want extract from the map
--- filter_nodes.py
+++ filter_nodes.py
@@ -1,44 +1,39 @@
 from haversine import haversine, Unit
+from pathlib import Path
 import xml.etree.ElementTree as ET
 import re
 import os
 import subprocess
+import yaml
 
-working_path = "/home/reneas/Dokumente/OSM/Alpentour"
-pbf_file_name = "map_alpentour.pbf"
-
-poi_dict = {
-    "Camping": [300, "tourism.camp_pitch,tourism.camp_site,tourism.caravan_site"],
-    "Fahrrad": [100, "amenity.bicycle_repair_station,shop.bicycle"],
-    "Fastfood": [100, "shop.bakery,amenity.fast_food"],
-    "Hotel": [
-        200,
-        "tourism.hostel,tourism.hotel,tourism.motel,tourism.apartment,tourism.guest_house",
-    ],
-    "Hütte": [300, "tourism.alpine_hut,tourism.wilderness_hut"],
-    "Icecream": [100, "amenity.ice_cream"],
-    "Picknick": [1000, "amenity.bench,leisure.picnic_table,tourism.picnic_site"],
-    "Supermarkt": [100, "shop.convenience,shop.supermarket,shop.food"],
-    "Wasser": [150, "emergency.drinking_water,amenity.drinking_water,natural.spring"],
-}
+# TODO: Make the script more flexible by using command line arguments
 
 
-def extract_nodes(pbf_file, poi):
-    # If the directory doesn't exist, create it
-    if not os.path.exists(f"{working_path}/osm_raw"):
-        os.makedirs(f"{working_path}/osm_raw")
-    osmosis_command = f"osmosis --read-pbf {pbf_file} --node-key-value keyValueList={poi_dict[poi][1]} --write-xml {working_path}/osm_raw/{poi}.osm"
-    print(osmosis_command)
+#################### FUNCTIONS ####################
+def run_subprocess(command):
     process = subprocess.Popen(
-        osmosis_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
     )
     output, error = process.communicate()
-
-    # Print the output and error messages, if any
     if output:
         print("Output:", output.decode())
     if error:
         print("Error:", error.decode())
+
+
+def create_directory(path):
+    """
+    Create a directory and it's parents if they don't exist
+    """
+    path.mkdir(parents=True, exist_ok=True)
+
+
+def extract_nodes(pbf_file, poi, poi_dict, working_path):
+    osm_raw_path = working_path / "osm_raw"
+    create_directory(osm_raw_path)
+    osmosis_command = f"osmosis --read-pbf {pbf_file} --node-key-value keyValueList={poi_dict[poi][1]} --write-xml {osm_raw_path}/{poi}.osm"
+    print(osmosis_command)
+    run_subprocess(osmosis_command)
 
 
 def filter_nodes(xml_file, dist):
@@ -64,48 +59,50 @@ def filter_nodes(xml_file, dist):
     return root
 
 
-def write_xml(root, filtered_dir):
-    if not os.path.exists(filtered_dir):
-        os.makedirs(filtered_dir)
+def write_xml(root, working_path, poi):
+    osm_filtered_path = working_path / "osm_filtered"
+    create_directory(osm_filtered_path)
     # Open the output file in write mode
-    with open(f"{filtered_dir}/filtered_{poi}.osm", "w") as f:
+    with open(f"{osm_filtered_path}/filtered_{poi}.osm", "w") as f:
         # Write the XML declaration
         f.write("<?xml version='1.0' encoding='UTF-8'?>\n")
         # Write the tree to a string
         xml_string = ET.tostring(root, encoding="utf-8", method="xml")
         # Decode bytes to string
         xml_string = xml_string.decode("utf-8")
-        # Remove spaces before /> in empty tags
+        # Remove spaces before />
         xml_string = re.sub(" />", "/>", xml_string)
         # Write the modified XML string to the file
         f.write(xml_string)
 
 
-def create_gpi():
-    iconpath = f"{working_path}/Icons"
-    gpipath = f"{working_path}/GPI"
-    inputpath = f"{working_path}/osm_filtered"
-
-    if not os.path.exists(gpipath):
-        os.makedirs(gpipath)
-    gpsbabel_command = f"gpsbabel -i osm -f {inputpath}/filtered_{poi}.osm -o garmin_gpi,category={poi},bitmap={iconpath}/{poi}.bmp -F {gpipath}/{poi}.gpi"
+def create_gpi(working_path, poi):
+    iconpath = working_path / "Icons"
+    gpipath = working_path / "GPI"
+    osm_filtered_path = working_path / "osm_filtered"
+    create_directory(gpipath)
+    gpsbabel_command = f"gpsbabel -i osm -f {osm_filtered_path}/filtered_{poi}.osm -o garmin_gpi,category={poi},bitmap={iconpath}/{poi}.bmp -F {gpipath}/{poi}.gpi"
     print(gpsbabel_command)
-    process = subprocess.Popen(
-        gpsbabel_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
-    )
-    output, error = process.communicate()
-
-    if output:
-        print("Output:", output.decode())
-    if error:
-        print("Error:", error.decode())
+    run_subprocess(gpsbabel_command)
 
 
-for poi in poi_dict:
-    filtered_dir = f"{working_path}/osm_filtered"
-    extract_nodes(f"{working_path}/{pbf_file_name}", poi)
-    write_xml(
-        filter_nodes(f"{working_path}/osm_raw/{poi}.osm", poi_dict[poi][0]),
-        filtered_dir,
-    )
-    create_gpi()
+def main():
+    working_path = Path(__file__).parent
+    print(working_path)
+    pbf_file_name = "map_alpentour.pbf"
+
+    with open("POIs.yaml", "r") as poi_file:
+        poi_dict = yaml.safe_load(poi_file)
+
+    for poi in poi_dict:
+        extract_nodes(f"{working_path}/{pbf_file_name}", poi, poi_dict, working_path)
+        write_xml(
+            filter_nodes(f"{working_path}/osm_raw/{poi}.osm", poi_dict[poi][0]),
+            working_path,
+            poi,
+        )
+        create_gpi(working_path, poi)
+
+
+if __name__ == "__main__":
+    main()
